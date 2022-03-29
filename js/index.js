f4801187fd671ff53836e5284453d9d93b29550b
Andrei Yahorau <yahoraual92@gmail.com>
2022-03-29 11:51:42 +0300

    Corrected sending the data

--- js/index.js
+++ js/index.js
@@ -2,6 +2,22 @@ const modalWindow = document.querySelector('.modal');
 const modalButtons = document.querySelectorAll('.modal__button');
 const modalClose = modalWindow.querySelector('.modal__close');
 
+const sendForm = (name, phone, email) => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",
+        body: JSON.stringify({
+            name: name,
+            phone: phone,
+            email: email,
+        }),
+    })
+    .then(response => response.json())
+    .then(json => console.log("OK"))
+    .catch(err => console.log(err))
+    .finally(modalWindow.style.display = 'none');
+}
+
+
 modalButtons.forEach((item) => {
     item.addEventListener('click', () => {
         modalWindow.style.display = 'flex';
@@ -20,3 +36,7 @@ modalClose.addEventListener('click', () => {
     modalWindow.style.display = 'none';
 });
 
+modalWindow.addEventListener('submit', (event) => {
+    event.preventDefault();
+    sendForm(modalWindow.name.value, modalWindow.phone.value, modalWindow.email.value);
+});
